f627b7554d996cf11208969a8e7b1e5aa1a17a9b
Steve Munoz <stevomunoz@yahoo.com>
2025-06-11 13:30:25 -0500

    fixed footer to stay at the bottom between navigation of home page and about page

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -2,7 +2,7 @@ import react from 'react'
 
 function Footer(){
     return (
-        <div className = "bg-orange-200 h-14 inset-x-0 top-5 bottom-0 relative">
+        <div className = "bg-orange-200 h-14 inset-x-0 mt-5 bottom-0 fixed">
             <h1 className = "text-black text-bold mt-auto text-center p-4">&#160; 2025 #VANLIFE</h1>
         </div>
     )
--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,7 @@ export default function Home() {
   const styling = {backgroundImage: `url('${homeHero.src}')`}
   return (
     <>
-      <div style ={styling} className = 'home-container bg-center bg-cover min-h-350 text-white py-11 px-6'>
+      <div style ={styling} className = 'flex flex-col min-h-100 bg-center bg-cover text-white py-11 px-6'>
         <div className = "bg-[#FF8C38]/40 p-[10px] rounded-md">
           <h1 className = "font-bold text-3xl opacity-100 leading-10">You got the travel plans, we got the travel vans. </h1>
           <p className = "leading-6 font-bold"> Add adventure to your life by joining the #vanlife movement. Rent the perfect van to make your perfect road trip.</p>
