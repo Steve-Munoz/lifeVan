92573e5ab8db9043a1d2174d4c7de5e5d156374d
Steve Munoz <stevomunoz@yahoo.com>
2025-06-12 14:44:49 -0500

    created mock api server for vans

--- src/app/server.js
+++ src/app/server.js
@@ -1,9 +1,11 @@
-import {createServer, Model} from "mirage.js"
+import { createServer, Model } from "miragejs"
+
 
 createServer({
     models: {
         vans: Model,
     },
+
     seeds(server) {
         server.create("van", { id: "1", name: "Modest Explorer", price: 60, description: "The Modest Explorer is a van designed to get you out of the house and into nature. This beauty is equipped with solar panels, a composting toilet, a water tank and kitchenette. The idea is that you can pack up your home and escape for a weekend or even longer!", imageUrl: "https://assets.scrimba.com/advanced-react/react-router/modest-explorer.png", type: "simple", hostId:"123" })
         server.create("van", { id: "2", name: "Beach Bum", price: 80, description: "Beach Bum is a van inspired by surfers and travelers. It was created to be a portable home away from home, but with some cool features in it you won't find in an ordinary camper.", imageUrl: "https://assets.scrimba.com/advanced-react/react-router/beach-bum.png", type: "rugged", hostId:"123" })
@@ -12,7 +14,8 @@ createServer({
         server.create("van", { id: "5", name: "The Cruiser", price: 120, description: "The Cruiser is a van for those who love to travel in comfort and luxury. With its many windows, spacious interior and ample storage space, the Cruiser offers a beautiful view wherever you go.", imageUrl: "https://assets.scrimba.com/advanced-react/react-router/the-cruiser.png", type: "luxury", hostId:"789" })
         server.create("van", { id: "6", name: "Green Wonder", price: 70, description: "With this van, you can take your travel life to the next level. The Green Wonder is a sustainable vehicle that's perfect for people who are looking for a stylish, eco-friendly mode of transport that can go anywhere.", imageUrl: "https://assets.scrimba.com/advanced-react/react-router/green-wonder.png", type: "rugged", hostId:"123"  })
     },
-     routes() {
+
+    routes() {
         this.namespace = "api"
         this.logging = false
 
