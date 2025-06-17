7ed786b430c67858e034936037f8f5f9cbf44067
Steve Munoz <stevomunoz@yahoo.com>
2025-06-17 11:05:51 -0500

    van details page created using UseState, UseEffect, fetching api and using params for vanid

---
+++ src/app/vans/[vanid]/page.js
@@ -0,0 +1,30 @@
+"use client"
+import React, {useEffect, useState} from "react"
+import "../../server"
+
+
+export default function VanDetails({params}){
+    const [vanDetails, setVanDetails] = useState(null)
+    useEffect(()=>{fetch(`/api/vans/${params.vanid}`).then(res=>res.json()).then(data=>setVanDetails(data.vans))},[])
+    return(
+        <>
+        <div>
+            <h1>Van Details: ID: {params.vanid}</h1>
+            {vanDetails ? (
+                <div>
+                    <img src = {vanDetails.imageUrl}/>
+                    <i className = {`van-type ${vanDetails.type}`}>{vanDetails.type}</i>
+                    <h2>{vanDetails.name}</h2>
+                    <p>${vanDetails.price}<span>/day</span></p>
+                    <p>{vanDetails.description}</p>
+                    <button className ="link-button p-[5px] font-bold bg-[#FF8C38] rounded-md">Rent this</button>
+                </div>
+
+            ): <h2>Loading..</h2>
+            }   
+           
+
+        </div>
+        </>
+    )
+}
